f92ed1b9114bec838b1d375de0d151b71eb13783
Gbolahan Sholola <83152907+Sholola-Gbolahan@users.noreply.github.com>
2024-05-17 21:53:03 +0100

    Added Mail Examples from github to project

---
+++ app/(admin)/mail/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { cookies } from "next/headers"
+
+import { Mail } from './components/mail'
+import { accounts,mails } from './data'
+
+export default function page() {
+    const layout = cookies().get("react-resizable-panels:layout")
+  const collapsed = cookies().get("react-resizable-panels:collapsed")
+
+    const defaultLayout = layout ? JSON.parse(layout.value) : undefined
+    const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined
+
+
+  return (
+    <div>
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+    </div>
+  )
+}
